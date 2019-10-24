ce50a0ec892f3bed1eec0fef388e678539142ed0
Rodrigo Villalobos <rodrigovillalobos@MacBook-Pro-de-KC211095.local>
2019-10-24 07:25:02 -0500

    first commit

---
+++ src/main/java/com/iteso/decorator/condiments/meat/FlorJamaica.java
@@ -0,0 +1,29 @@
+package com.iteso.decorator.condiments.meat;
+
+import com.iteso.decorator.CondimentsDecorator;
+import com.iteso.decorator.Taco;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rvillalobos
+ * Date: 8/19/13
+ * Time: 4:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FlorJamaica extends CondimentsDecorator{
+    Taco taco;
+
+    public FlorJamaica(Taco taco){
+        this.taco = taco;
+    }
+
+    @Override
+    public String getDescription() {
+        return taco.getDescription() + " vegano";
+    }
+
+    @Override
+    public double cost() {
+        return 5 + taco.cost();
+    }
+}
